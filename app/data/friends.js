def595d2bfbe471d7d5507bb70613a33952dfac5
anthonyatticus <anthony@bounceid.com>
2019-04-13 18:49:45 -0700

    front end done

---
+++ app/data/friends.js
@@ -0,0 +1,164 @@
+var friendsArray = [
+	{
+		name: 'Anthony Atticus',
+		photo: 'https://i2-prod.mirror.co.uk/incoming/article7961698.ece/ALTERNATES/s615b/PROD-Overweight-man-with-fat-stomach.jpgd',
+		scores: [
+			'1',
+			'3',
+			'4',
+			'2',
+			'1',
+			'3',
+			'5',
+			'3',
+			'4',
+			'5'
+		]
+	},
+	{
+		name: 'Kaitlyn Gender',
+		photo: 'https://i.imgur.com/p9R6v1U.jpg?1',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Brent',
+		photo: 'https://hips.hearstapps.com/esquireuk.cdnds.net/15/37/2048x2730/2048x2730-brad-pitt-fight-club-2016-43-jpg-f2342fb7.jpg?resize=480:*',
+		scores: [
+			'4',
+			'2',
+			'1',
+			'3',
+			'4',
+			'5',
+			'4',
+			'1',
+			'4',
+			'1'
+		]
+	},
+	{
+		name: 'Kayla',
+		photo: 'https://i.pinimg.com/originals/33/10/2c/33102ca1ebc3e5c9f6ba7ac60a6fb0b5.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Cloe',
+		photo: 'https://i9.dainikbhaskar.com/thumbnails/680x588/web2images/english.fashion101.in/2015/11/20/a-model-at-dsquard-2_1448.jpg',
+		scores: [
+			'2',
+			'2',
+			'3',
+			'3',
+			'4',
+			'4',
+			'5',
+			'5',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Sean',
+		photo: 'http://lanceyork.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/a/t/athletic_trunk_white_front_1.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Ashley',
+		photo: 'https://i.pinimg.com/originals/0f/a2/7c/0fa27cd850fe0377285f0d1122f26c87.jpg',
+		scores: [
+			'1',
+			'5',
+			'2',
+			'1',
+			'2',
+			'5',
+			'3',
+			'4',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Gymy Johns',
+		photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREtjThNm8MhDXMZpoWKnpk2dkPB8BBuLRglrRyolqCgXBQYd7vKA',
+		scores: [
+			'2',
+			'1',
+			'2',
+			'7',
+			'5',
+			'3',
+			'5',
+			'4',
+			'2',
+			'4'
+		]
+	},
+	{
+		name: 'Harry Spotter',
+		photo: 'https://wonderjunkiesblog.files.wordpress.com/2017/06/fat_2.jpg?w=1000',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Jessica (girl next door)',
+		photo: 'https://justnewsviews.com/wp-content/uploads/2018/09/1537697261_maxresdefault.jpg',
+		scores: [
+            '1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	}
+];
+
+module.exports = friendsArray;
